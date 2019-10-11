9790c0cc5c43e073f4f918b8562a3bebce6ac60a
kate-gill <kategill@email.com>
2019-10-11 13:09:54 +0100

    Updates

---
+++ precache-manifest.94f98dc401b1226c66e4aaf4347e0ef4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555b4a1a9ec96f7d5917327c50cb7583",
+    "url": "/sticky-notes-dashboard/index.html"
+  },
+  {
+    "revision": "7a275ee2582d5f2d5018",
+    "url": "/sticky-notes-dashboard/static/css/main.37dce1ee.chunk.css"
+  },
+  {
+    "revision": "86d45909683dba754e4f",
+    "url": "/sticky-notes-dashboard/static/js/2.a6271471.chunk.js"
+  },
+  {
+    "revision": "7a275ee2582d5f2d5018",
+    "url": "/sticky-notes-dashboard/static/js/main.feb17882.chunk.js"
+  },
+  {
+    "revision": "750f73a632d1a8be89ea",
+    "url": "/sticky-notes-dashboard/static/js/runtime~main.8831c7ee.js"
+  }
+]);
